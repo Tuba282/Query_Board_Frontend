5ed11ed95db55a3b16ba5ecfe50bc652dba75a4e
Tuba282 <tubajan282@gmail.com>
2026-04-18 01:44:34 +0500

    add server link

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://query-board-server.vercel.app/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://query-board-backend.vercel.app/api';
 
 const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
